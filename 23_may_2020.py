b7428629a0a0a6de23a09a904aba06732424a3b0
Naveen <64359307+naveenholehonnur@users.noreply.github.com>
2020-05-24 01:49:32 +0530

    Update 23_may_2020.py

--- 23_may_2020.py
+++ 23_may_2020.py
@@ -31,10 +31,41 @@ while True:
     x+=1
 print(op[n-1])
             
+   
+
+
+
+# Question-2
+
+# Alice has a hand of cards, given as array of integers. Now she wants to arrange the cards into groups so that each group of size W and consists of W consecutive cards. Return true if we can split that.
+# hand=[1,2,3,6,2,3,4,7,8]
+# w=3
+# Output: true
+# Explanation: [1,2,3],[2,3,4],[6,7,8]
         
         
-        
-        
+ # Solution :--
+
+w=int(input('enter group of card size='))
+hand = list(map(int,input('enter card numbers=').strip(' []').split(',')))
+op = []
+for i in range(int(len(hand)/w)):
+        temp=sorted(list(set(hand)))
+        op.append(temp[:w])
+        check = temp[0]
+        for j in temp[:w]:
+            if check!=j:
+                print('false')
+                break
+            check+=1
+            hand.remove(j)
+        else:
+            continue
+        break
+else:
+    print('true')
+    print(op)
+    
 
 
 
